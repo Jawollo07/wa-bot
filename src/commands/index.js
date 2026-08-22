360b840d9c82993a35848ccb9257e867f820a197
Jawollo07 <jannik@mcjj.de>
2026-08-22 12:57:18 +0200

    fix: expose command default export

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,2 +1,4 @@
-/** Command boundary. Command implementations can be split into domain modules without changing the bot transport. */
-export { default as handleAdminCommands } from '../../commands.js';
+/** Command boundary. */
+import handleAdminCommands from '../../commands.js';
+export { handleAdminCommands };
+export default handleAdminCommands;
