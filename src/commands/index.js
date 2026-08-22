c10653ccf30b37930ba43a2410e05e7d5dc3df37
Jawollo07 <jannik@mcjj.de>
2026-08-22 12:56:02 +0200

    refactor: add command module boundary

---
+++ src/commands/index.js
@@ -0,0 +1,2 @@
+/** Command boundary. Command implementations can be split into domain modules without changing the bot transport. */
+export { default as handleAdminCommands } from '../../commands.js';
